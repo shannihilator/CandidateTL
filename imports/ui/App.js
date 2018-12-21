b007d558193639e02acfdc9dde61b9a8084815d4
anonymous <anonymous>
2018-12-20 20:23:13 -0500

    Letter count working and displaying
    Added letter counting algorithm and elements.
    Todo: Move API call into APP.js and break out lettercount table as unique component.

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -5,6 +5,21 @@ import People  from './People.js';
 // App component - represents the whole app
 class App extends Component {
 
+    constructor(props){
+    super(props);
+    this.state = {
+      people: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => this.setState({ people: data }));
+      console.log(this.state.people);
+  }
+
+
   render() {
     return (
       <div className="container">
--- imports/ui/LetterTable.js
+++ imports/ui/LetterTable.js
@@ -1,23 +1,12 @@
 import React, { Component } from 'react';
 
-const API = 'https://jsonplaceholder.typicode.com/users';
-
-class People extends Component {
+class LetterTable extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       people: [],
-    }; 
-  }
-
-  //someFn = this.props.callbackFromParent('2');
-
-  componentDidMount() {
-    fetch(API)
-      .then(response => response.json())
-      .then(data => this.setState({ people: data }));
-     
+    };
   }
 
     render() {
@@ -35,14 +24,12 @@ class People extends Component {
         letterCounts.push({ "char": x, "count": y});
         emailAddresses.splice(0,y);
     }
-    
+
     letterCounts.sort(function (a, b) { return b.count - a.count});
-    //this.props.callbackFromParent(letterCounts);
-    console.log(emailAddresses);
-    console.log(letterCounts);
-    //this.props.callbackFromParent('2');
+   // console.log(emailAddresses);
+   // console.log(letterCounts);
+
     return (
-      <div>
         <table>
         <tbody>
         <tr>
@@ -62,28 +49,8 @@ class People extends Component {
         </tr>
         </tbody>
         </table>
-
-        <table>
-        <tbody>
-        <tr>
-        <th>Character</th>
-        <th>Count</th>
-        </tr>
-        {letterCounts.map(item =>
-          <tr key={item.char}>
-            <td>{item.char}</td>
-            <td>{item.count}</td>
-          </tr>
-        )} 
-        <tr>
-        <td></td>
-        </tr>
-        </tbody>
-        </table>
-        </div>
-
         );
   }
 }
 
-export default People;
+export default LetterTable;
