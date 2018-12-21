99538329a4d4bafd30d10dea212310ef611254a1
anonymous <anonymous>
2018-12-20 23:37:25 -0500

    Clean up App.js and install ssl package

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -6,42 +6,56 @@ import LetterTable from './LetterTable.js';
 // App component - represents the whole app
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      people: [],
-      showLetterCount: false,
+    constructor(props) {
+        super(props);
+        this.state = {
+            people: [],
+            showLetterCount: false,
+        }
+
+        this.handleClick = this.handleClick.bind(this);
     }
 
-    this.handleClick = this.handleClick.bind(this);
-  }
+    handleClick() {
+        this.setState(state => ({
+            showLetterCount: !state.showLetterCount
+        }));
+    }
 
-  handleClick() {
-    this.setState(state => ({
-      showLetterCount: !state.showLetterCount
-    }));
-  }
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users'//, {
+            // method: 'GET',
+            //  withCredentials: true,
+            //  credentials: 'include',
+            //   headers: {
+            //       'Authorization': 'Bearer ',
+            //      'Content-Type': 'application/json',
+            // }
+            //}
+        )
+            .then(response => response.json())
+            .then(data => this.setState({ people: data }))
+            .catch((error) => {
+                console.error(error)
+            });
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data => this.setState({ people: data }));
-    //console.log(this.state.people);
-  }
+    }
 
-  render() {
-    return (
-      <div className="container">
-        <People people={this.state.people} />
-        <button onClick={this.handleClick}>
-          {this.state.showLetterCount ? 'HIDE LETTER COUNT' : 'SHOW LETTER COUNT'}
-        </button>
-        {this.state.showLetterCount && 
-        <LetterTable people={this.state.people} />
-        }
-      </div>
-    );
-  }
+    render() {
+        console.log(this.state.people);
+        return (
+
+            <div className="container">
+                <People people={this.state.people} />
+                <button onClick={this.handleClick}>
+                    {this.state.showLetterCount ? 'HIDE LETTER COUNT' : 'SHOW LETTER COUNT'}
+                </button>
+                {this.state.showLetterCount &&
+                    <LetterTable people={this.state.people} />
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
